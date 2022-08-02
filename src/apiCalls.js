0fa6aa28636376e9a86015206cae83abfa6aa402
Jordan Farelli <jordanfarelli@gmail.com>
2022-08-02 08:15:15 -0600

    Created GET request function and made functional

---
+++ src/apiCalls.js
@@ -0,0 +1,18 @@
+export let getData = ( dataType ) => {
+    return fetch( `http://localhost:3001/api/v1/${ dataType }` )
+        .then( response => response.json( ) )
+        .catch( error => console.log( error ) ); // Need to add error handling
+}
+
+
+// export let postData = ( data ) => {
+//     return fetch( 'http://localhost:3001/api/v1/users', {
+//         method: 'POST',
+//         body: JSON.stringify( data ),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//     .then( response => response.json( ) )
+//     .catch( error => console.log( error ) ); // Need to add error handling
+// }
