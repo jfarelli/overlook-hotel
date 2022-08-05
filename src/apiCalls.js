be6c2edf3b751490c2218d2a6e02872c36b8d650
Jordan Farelli <jordanfarelli@gmail.com>
2022-08-05 16:47:07 -0600

    Created postData function for room booking

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,14 +5,14 @@ export let getData = ( dataType ) => {
 }
 
 
-// export let postData = ( data ) => {
-//     return fetch( 'http://localhost:3001/api/v1/users', {
-//         method: 'POST',
-//         body: JSON.stringify( data ),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     })
-//     .then( response => response.json( ) )
-//     .catch( error => console.log( error ) ); // Need to add error handling
-// }
+export let postData = ( data ) => {
+    return fetch( 'http://localhost:3001/api/v1/bookings', {
+        method: 'POST',
+        body: JSON.stringify( data ),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then( response => response.json( ) )
+    .catch( error => console.log( error ) ); // Need to add error handling
+}
